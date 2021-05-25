f0122004f29d158813b21a8d2d5effce3bfcbc1f
root <root@debian>
2021-05-25 11:02:51 +0200

    modif .sh

--- testgit.sh
+++ testgit.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "bonjour"
-echo "le nom du pc est" $(hostname) "et celui du compte actif :" $(whoiam)
+echo "le nom du pc est" $(hostname) "et celui du compte actif :" $(whoami)
 python script.py
